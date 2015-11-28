05dcfe10c132fee741cef6b9a6e3ab80d2e77ff0
biel <bielamengualcoll@gmail.com>
2015-11-28 17:14:49 +0100

    ja tenc  els layputs principals he d'ajustar resolucions i acabar de pulir tota la presentació de cada layout

--- app/src/main/java/com/example/biel/autentic/central.java
+++ app/src/main/java/com/example/biel/autentic/central.java
@@ -3,8 +3,10 @@ package com.example.biel.autentic;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 /**
  * Created by biel on 28/11/15.
@@ -18,9 +20,22 @@ public class central extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.central);
 
+//referenciam les imatges que tenim al central.xml per fer les botons
 
+        ImageView imatge1 = (ImageView) findViewById(R.id.imageButton4);
+        //Per quan pitgin l'imatge faci algo
+        imatge1.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
 
+                // dins parentesis posam sempre on esteim i on anam
+                Intent fitxa_plat = new Intent(central.this, fitxa_plat.class);
+                //començam activitat i entre parentesi el nom del Inent creat fitxa_plat
+                startActivity(fitxa_plat);
 
+                return false; //ve per defecta ja veurem si el posam a true
+            }
+        });
 
 
 
---
+++ app/src/main/java/com/example/biel/autentic/fitxa_plat.java
@@ -0,0 +1,28 @@
+package com.example.biel.autentic;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+
+/**
+ * Created by biel on 28/11/15.
+ */
+public class fitxa_plat extends AppCompatActivity  {
+
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            //Aquest fitxa plat és el xml
+            setContentView(R.layout.fitxa_plat);
+
+
+
+        }
+
+
+
+}
