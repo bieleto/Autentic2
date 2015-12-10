75960fe87e3388a4be46353ac05d90366c89f06c
biel <bielamengualcoll@gmail.com>
2015-12-10 01:10:11 +0100

    He posat el menu adalt al action bar principal retocant el MainActivity.java introduint un onCreateOptionsMenu
    També he creat el menu_principal.xml dins res/menu on aniré ficant tots els botons que vull que apareguin i ja crearé la seva funcionalitat dins Main Activity o millor ho puc fer a un .java diferent.

--- app/src/main/java/com/example/biel/autentic/MainActivity.java
+++ app/src/main/java/com/example/biel/autentic/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.biel.autentic;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
-
-
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 
 //És la pantalla principal on surtirà el menu principal que seran X botons.
@@ -16,6 +18,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//Afegit dia 9 12 15 se podrà borrar segurament//
+        //he de crear el toolbar inicial d'alt on posaré la cerca mapa foto refresca.
+        //Aquesta declaració necessita fer
+       // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar)
+////////////////////////////////////////////
+
+
+
+
+
         //Referenciam el primer boto de la vista xml fer-ho 4 pics per el 4 botons
         ImageButton botocentral = (ImageButton) findViewById(R.id.botocentral);
 
@@ -36,10 +48,35 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+/*Afegit dia 9 12 15 se podrà borrar segurament
+@Override
+    public boolean onOptionsItemSelection (MenuItem item){
+//Elements de la barra d'acció principal. Botons on podrem clicar
+// Com hem espicifat una activitat principal al Android Manifest
+    int id = item.getItemId();
+    //Feim els casos del que poden pitjar
+    switch (item.getItemId()){
+        case R.id.action_map:
+            Toast.makeText(this, "Acción Mapa", Toast.LENGTH_SHORT).show();
+            return true;
+
+        default:
+            return super.onOptionsItemSelected(item);
+
+    }
 
 
+}
 
+Afegit dia 9 12 15 se podrà borrar segurament */
 
+    // 9-12-15 Intent fer un submenú en botons a la action bar principal on hi ha el nom del programa.
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu){
+        //Farem un Inflate que en teoria ficarà els items que volguem dins la action bar que ja tenim
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        return true;
+    }
 
 
 }
